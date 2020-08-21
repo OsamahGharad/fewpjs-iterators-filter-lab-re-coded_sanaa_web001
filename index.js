6914e6901aa14e5144094d6a0871c46ceaf71d87
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-21 00:21:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function findMatching(drivers,name){
  return drivers.filter(driver =>{
     return driver.toLowerCase() === name.toLowerCase();
-  });
+  })
 
 }
+
 function fuzzyMatch(drivers,name){
   return drivers.filter(driver =>  driver.slice(0,name.length)===name)
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name == name);
 }
