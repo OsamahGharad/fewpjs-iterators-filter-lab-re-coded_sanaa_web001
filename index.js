bfad450e4fb6de497953ce30e3aea2db4466de9e
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-21 00:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 function findMatching(drivers,name){
  return drivers.filter(driver =>{
-     driver.toLowerCase() === name.toLowerCase();
+    return driver.toLowerCase() === name.toLowerCase();
   });
 
 }
-// function fuzzyMatch(drivers,name){
-//   return drivers.filter(driver =>{
-//     driver.slice(0,name.length)===name
-//   })
-// }
-const fuzzyMatch = (arr, string) => {
-  return arr.filter(n => n.slice(0, string.length) === string)
+function fuzzyMatch(drivers,name){
+  return drivers.filter(driver =>  driver.slice(0,name.length)===name)
 }
